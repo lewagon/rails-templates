b13e0fb03a9d9643f36f1ca84fcdcfd18d50616d
Sebastien Saunier <seb@saunier.me>
2015-08-25 17:26:59 +0200

    Adding autoprefixer

--- devise.rb
+++ devise.rb
@@ -15,6 +15,7 @@ gem 'uglifier'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   gem 'binding_of_caller'
--- minimal.rb
+++ minimal.rb
@@ -14,6 +14,7 @@ gem 'uglifier'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   gem 'binding_of_caller'
