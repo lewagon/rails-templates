ed9b022a2a436be70b249c757982946d88ed7a66
Cecile Veneziani <contact@cecilitse.org>
2017-04-11 11:50:33 +0900

    Initialize Clever Cloud folder

---
+++ clever_cloud/devise.rb
@@ -0,0 +1,219 @@
+run "pgrep spring | xargs kill -9"
+
+# GEMFILE
+########################################
+run "rm Gemfile"
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+ruby '#{RUBY_VERSION}'
+
+gem 'rails', '#{Rails.version}'
+gem 'puma'
+gem 'pg'
+gem 'figaro'
+gem 'jbuilder', '~> 2.0'
+gem 'devise'
+gem 'redis'
+
+gem 'sass-rails'
+gem 'jquery-rails'
+gem 'uglifier'
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+
+group :development, :test do
+  gem 'binding_of_caller'
+  gem 'better_errors'
+  #{Rails.version >= "5" ? nil : "gem 'quiet_assets'"}
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'spring'
+  #{Rails.version >= "5" ? "gem 'listen', '~> 3.0.5'" : nil}
+  #{Rails.version >= "5" ? "gem 'spring-watcher-listen', '~> 2.0.0'" : nil}
+end
+
+#{Rails.version < "5" ? "gem 'rails_12factor', group: :production" : nil}
+RUBY
+
+# Ruby version
+########################################
+file ".ruby-version", RUBY_VERSION
+
+# Procfile
+########################################
+file 'Procfile', <<-YAML
+web: bundle exec puma -C config/puma.rb
+YAML
+
+# Spring conf file
+########################################
+inject_into_file 'config/spring.rb', before: ').each { |path| Spring.watch(path) }' do
+  "  config/application.yml\n"
+end
+
+# Puma conf file
+########################################
+if Rails.version < "5"
+puma_file_content = <<-RUBY
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+
+threads     threads_count, threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+RUBY
+
+file 'config/puma.rb', puma_file_content, force: true
+end
+
+# Assets
+########################################
+run "rm -rf app/assets/stylesheets"
+run "curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip"
+run "unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets"
+
+run 'rm app/assets/javascripts/application.js'
+file 'app/assets/javascripts/application.js', <<-JS
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require_tree .
+JS
+
+# Dev environment
+########################################
+gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+
+# Layout
+########################################
+run 'rm app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.erb', <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO</title>
+    <%= csrf_meta_tags %>
+    #{Rails.version >= "5" ? "<%= action_cable_meta_tag %>" : nil}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <%= stylesheet_link_tag    'application', media: 'all' %>
+  </head>
+  <body>
+    <%= render 'shared/navbar' %>
+    <%= render 'shared/flashes' %>
+    <%= yield %>
+    <%= javascript_include_tag 'application' %>
+  </body>
+</html>
+HTML
+
+file 'app/views/shared/_flashes.html.erb', <<-HTML
+<% if notice %>
+  <div class="alert alert-info alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <%= notice %>
+  </div>
+<% end %>
+<% if alert %>
+  <div class="alert alert-warning alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <%= alert %>
+  </div>
+<% end %>
+HTML
+
+run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb"
+run "curl -L https://raw.githubusercontent.com/lewagon/design/master/logos/png/logo_red_circle.png > app/assets/images/logo.png"
+
+# README
+########################################
+markdown_file_content = <<-MARKDOWN
+Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+MARKDOWN
+file 'README.md', markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<-RUBY
+config.generators do |generate|
+      generate.assets false
+    end
+RUBY
+
+environment generators
+
+########################################
+# AFTER BUNDLE
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rake 'db:drop db:create db:migrate'
+  generate('simple_form:install', '--bootstrap')
+  generate(:controller, 'pages', 'home', '--no-assets', '--skip-routes')
+
+  # Routes
+  ########################################
+  route "root to: 'pages#home'"
+
+  # Git ignore
+  ########################################
+  run "rm .gitignore"
+  file '.gitignore', <<-TXT
+.bundle
+log/*.log
+tmp/**/*
+tmp/*
+*.swp
+.DS_Store
+public/assets
+TXT
+
+  # Devie install + user
+  ########################################
+  generate('devise:install')
+  generate('devise', 'User')
+
+  # App controller
+  ########################################
+  run 'rm app/controllers/application_controller.rb'
+  file 'app/controllers/application_controller.rb', <<-RUBY
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+end
+RUBY
+
+  # migrate + devie views
+  ########################################
+  rake 'db:migrate'
+  generate('devise:views')
+
+  # Pages Controller
+  ########################################
+  run 'rm app/controllers/pages_controller.rb'
+  file 'app/controllers/pages_controller.rb', <<-RUBY
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :home ]
+
+  def home
+  end
+end
+RUBY
+
+  # Environments
+  ########################################
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
+
+  # Figaro
+  ########################################
+  run "figaro install"
+
+  # Git
+  ########################################
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit with devise template from https://github.com/lewagon/rails-templates' }
+end
