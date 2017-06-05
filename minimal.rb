a0d26ac9b6b638296acc5e175498ab55c5e58a06
Alex Benoit <alexander.benoit@outlook.com>
2017-06-05 21:28:48 +0100

    done normal templates

--- minimal.rb
+++ minimal.rb
@@ -25,15 +25,12 @@ gem 'autoprefixer-rails'
 group :development, :test do
   gem 'binding_of_caller'
   gem 'better_errors'
-  #{Rails.version >= "5" ? nil : "gem 'quiet_assets'"}
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'spring'
-  #{Rails.version >= "5" ? "gem 'listen', '~> 3.0.5'" : nil}
-  #{Rails.version >= "5" ? "gem 'spring-watcher-listen', '~> 2.0.0'" : nil}
+  gem 'listen', '~> 3.0.5'
+  gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
-#{Rails.version < "5" ? "gem 'rails_12factor', group: :production" : nil}
 RUBY
 
 # Ruby version
@@ -50,20 +47,6 @@ YAML
 ########################################
 inject_into_file 'config/spring.rb', before: ').each { |path| Spring.watch(path) }' do
   "  config/application.yml\n"
-end
-
-# Puma conf file
-########################################
-if Rails.version < "5"
-puma_file_content = <<-RUBY
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-
-threads     threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-RUBY
-
-file 'config/puma.rb', puma_file_content, force: true
 end
 
 # Assets
@@ -91,13 +74,13 @@ file 'app/views/layouts/application.html.erb', <<-HTML
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta charset="UTF-8">
     <title>TODO</title>
     <%= csrf_meta_tags %>
-    #{Rails.version >= "5" ? "<%= action_cable_meta_tag %>" : nil}
-    <%= stylesheet_link_tag    'application', media: 'all' %>
+    <%= action_cable_meta_tag %>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <%= stylesheet_link_tag 'application', media: 'all' %>
   </head>
   <body>
     <%= yield %>
@@ -132,7 +115,7 @@ after_bundle do
   ########################################
   rake 'db:drop db:create db:migrate'
   generate('simple_form:install', '--bootstrap')
-  generate(:controller, 'pages', 'home', '--no-helper', '--no-assets', '--skip-routes')
+  generate(:controller, 'pages', 'home', '--skip-routes')
 
   # Routes
   ########################################
