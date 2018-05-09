65721e3459ce8daae6d2c4954f4c8f7a059e3fc5
Arthur Littmann <arthur.littmann@gmail.com>
2018-05-09 10:13:23 +0100

    update fontawesome gem version

--- clever_cloud/minimal.rb
+++ clever_cloud/minimal.rb
@@ -17,7 +17,7 @@ gem 'redis'
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
-gem 'font-awesome-sass', '~> 4.7'
+gem 'font-awesome-sass', '~> 5.0.9'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
