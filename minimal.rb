a54bf4ae35e6f8331c7788497400fe48c4d2a4e7
Germain Loret <germain.loret@gmail.com>
2018-05-24 19:48:07 +0200

    update path

--- minimal.rb
+++ minimal.rb
@@ -64,7 +64,7 @@ run 'rm -rf app/assets/stylesheets'
 run 'rm -rf vendor'
 run 'curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip'
 run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
-inject_into_file 'app/assets/stylesheets/application.scss', before: '// Override other variables below!' do
+inject_into_file 'app/assets/stylesheets/config/_bootstrap_variables.scss', before: '// Override other variables below!' do
   "
   // Patch to make simple_form compatible with bootstrap 3
   .invalid-feedback {
