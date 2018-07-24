b6ce7eb737304f857a5247d814443a0cccc1ea9a
Edward Schults <edward.schults@gmail.com>
2018-07-24 12:25:01 +0200

    remove figaro

--- clever_cloud/minimal.rb
+++ clever_cloud/minimal.rb
@@ -8,7 +8,6 @@ source 'https://rubygems.org'
 ruby '#{RUBY_VERSION}'
 
 #{"gem 'bootsnap', require: false" if Rails.version >= "5.2"}
-gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
 gem 'puma'
