dbe337702b91c4455ce3fcfd76e9f42c3eea9c03
Edward Schults <edward.schults@gmail.com>
2020-01-09 15:16:22 +0100

    remove update of asset's pipeline app.js

--- minimal.rb
+++ minimal.rb
@@ -12,10 +12,11 @@ inject_into_file 'Gemfile', before: 'group :development, :test do' do
 end
 
 inject_into_file 'Gemfile', after: 'group :development, :test do' do
-  <<~RUBY
-      gem 'pry-byebug'
-      gem 'pry-rails'
-      gem 'dotenv-rails'
+  <<-RUBY
+
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'dotenv-rails'
   RUBY
 end
 
@@ -31,14 +32,6 @@ run 'rm -rf app/assets/stylesheets'
 run 'rm -rf vendor'
 run 'curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip'
 run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
-
-if Rails.version < "6"
-  run 'rm app/assets/javascripts/application.js'
-  file 'app/assets/javascripts/application.js', <<~JS
-    //= require rails-ujs
-    //= require_tree .
-  JS
-end
 
 # Dev environment
 ########################################
