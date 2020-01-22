c8e79e6b93fccab8b366bf4dc5b031fb568d3d0c
Edward Schults <edward.schults@gmail.com>
2020-01-22 20:11:17 +0100

    replace quotes

--- devise.rb
+++ devise.rb
@@ -1,4 +1,4 @@
-run '[[ `uname` =~ "Darwin" ]] && pgrep spring | xargs kill -9'
+run "[[ `uname` =~ 'Darwin' ]] && pgrep spring | xargs kill -9"
 
 # GEMFILE
 ########################################
