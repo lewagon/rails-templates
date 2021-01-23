df6785131f72e03d6ee4de3482068e4e2304c9c3
Cecile Veneziani <contact@cecilitse.org>
2021-01-23 16:00:30 +0100

    Add irbrc config file to clever cloud templates

--- clever_cloud/devise.rb
+++ clever_cloud/devise.rb
@@ -24,6 +24,28 @@ end
 
 gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
 
+# IRB conf file
+########################################
+irbrc = '
+if defined?(Rails)
+  banner = ''
+
+  if Rails.env.production?
+    banner = "\e[41;97;1m prod \e[0m "
+  elsif Rails.env.staging?
+    banner = "\e[43;97;1m staging \e[0m "
+  end
+
+
+  IRB.conf[:PROMPT][:CUSTOM] = IRB.conf[:PROMPT][:DEFAULT].merge(
+    PROMPT_I: "#{banner}#{IRB.conf[:PROMPT][:DEFAULT][:PROMPT_I]}"
+  )
+
+  IRB.conf[:PROMPT_MODE] = :CUSTOM
+end
+'
+file '.irbrc', irbrc.strip
+
 # Clevercloud conf file
 ########################################
 file 'clevercloud/ruby.json', <<~EOF
--- clever_cloud/minimal.rb
+++ clever_cloud/minimal.rb
@@ -20,6 +20,28 @@ inject_into_file 'Gemfile', after: 'group :development, :test do' do
   RUBY
 end
 
+# IRB conf file
+########################################
+irbrc = '
+if defined?(Rails)
+  banner = ''
+
+  if Rails.env.production?
+    banner = "\e[41;97;1m prod \e[0m "
+  elsif Rails.env.staging?
+    banner = "\e[43;97;1m staging \e[0m "
+  end
+
+
+  IRB.conf[:PROMPT][:CUSTOM] = IRB.conf[:PROMPT][:DEFAULT].merge(
+    PROMPT_I: "#{banner}#{IRB.conf[:PROMPT][:DEFAULT][:PROMPT_I]}"
+  )
+
+  IRB.conf[:PROMPT_MODE] = :CUSTOM
+end
+'
+file '.irbrc', irbrc.strip
+
 # Clevercloud conf file
 ########################################
 file 'clevercloud/ruby.json', <<~EOF
