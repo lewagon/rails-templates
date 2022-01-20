a1fb8f179a52b29362bf61a68eaad7dc5f957d28
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-01-20 12:29:50 +0100

    update folder name

--- devise.rb
+++ devise.rb
@@ -27,7 +27,7 @@ gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
 run 'rm -rf app/assets/stylesheets'
 run 'rm -rf vendor'
 run 'curl -L https://github.com/lewagon/rails-stylesheets/archive/no-update.zip > stylesheets.zip'
-run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
+run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-no-updatemaster app/assets/stylesheets'
 
 # Dev environment
 ########################################
