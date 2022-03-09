8f61f763136998b250fec06360d067b8b072df07
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-03-09 16:38:48 +0100

    change name

--- devise.rb
+++ devise.rb
@@ -27,7 +27,7 @@ gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
 run 'rm -rf app/assets/stylesheets'
 run 'rm -rf vendor'
 run 'curl -L https://github.com/lewagon/rails-stylesheets/archive/simple-form-bootstrap-5.zip > stylesheets.zip'
-run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
+run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/simple-form-bootstrap-5 app/assets/stylesheets'
 
 # Dev environment
 ########################################
