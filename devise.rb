c54d732e40a89e81cf4ef4e4e55de6bd671572cd
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-04-13 16:57:30 +0200

    update font-awesome-sass version

--- devise.rb
+++ devise.rb
@@ -6,7 +6,7 @@ inject_into_file "Gemfile", before: "group :development, :test do" do
   <<~RUBY
     gem "autoprefixer-rails", "10.2.5"
     gem "devise"
-    gem "font-awesome-sass"
+    gem "font-awesome-sass", "~> 6.1"
     gem "simple_form", github: "heartcombo/simple_form"
   RUBY
 end
