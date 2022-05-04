48970be00eabdaf0aadd98c9e9eab957059117f4
Damien Milon <37899667+dmilon@users.noreply.github.com>
2022-05-04 13:08:43 +0200

    unpin autoprefixer-rails gem

--- devise.rb
+++ devise.rb
@@ -4,8 +4,8 @@ run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 ########################################
 inject_into_file "Gemfile", before: "group :development, :test do" do
   <<~RUBY
-    gem "autoprefixer-rails", "10.2.5"
     gem "devise"
+    gem "autoprefixer-rails"
     gem "font-awesome-sass", "~> 6.1"
     gem "simple_form", github: "heartcombo/simple_form"
   RUBY
