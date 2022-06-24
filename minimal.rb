2be95b3e615e8c8d483480fb2e5c2e6f5a8a2d30
Xun Ma <lazybone1995@hotmail.com>
2022-06-24 18:22:17 +0800

    include turbo-rails

--- minimal.rb
+++ minimal.rb
@@ -61,10 +61,10 @@ after_bundle do
       # Pin npm packages by running ./bin/importmap
 
       pin "application", preload: true
+      pin_all_from "app/javascript/components", under: "components"
       pin "vue", to: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.min.js'
       pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
       pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
-      pin_all_from "app/javascript/components", under: "components"
     RUBY
   end
 
