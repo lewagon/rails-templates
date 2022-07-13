a259060f7a2b4c661580b3b4d85de17a86c81f0d
xun ma <lazybone1995@hotmail.com>
2022-07-13 16:12:32 +0800

    add `vue-turbolinks` to importmap

--- devise.rb
+++ devise.rb
@@ -93,6 +93,7 @@ after_bundle do
       pin_all_from "app/javascript/components", under: "components"
       pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
       pin "vue", to: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.min.js'
+      pin "vue-turbolinks", to: "https://ga.jspm.io/npm:vue-turbolinks@2.2.2/index.js"
       pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
       pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
     RUBY
